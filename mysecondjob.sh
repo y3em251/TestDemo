059694bbca6d1c9ea1c92ac9857a86c01c3857e8
Santosh Kumar. Devara <santdeva@oradev.oraclecorp.com>
2018-07-23 16:46:43 +0530

    my sixth commit

--- mysecondjob.sh
+++ mysecondjob.sh
@@ -4,5 +4,5 @@ echo "oh my God , what is happenning"
 cd /tmp
 touch imp1 imp2 
 echo " This is the output result" >> imp1 
-echo " i am updating the second line" >> imp2 
+echo " i am updating the second line why " >> imp2 
 
