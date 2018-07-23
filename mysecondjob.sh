453dd2f212a6236525647814b6e75f74eeb5bfc8
Santosh Kumar. Devara <santdeva@oradev.oraclecorp.com>
2018-07-23 16:26:38 +0530

    my fifth commit

--- mysecondjob.sh
+++ mysecondjob.sh
@@ -3,5 +3,6 @@
 echo "oh my God , what is happenning"
 cd /tmp
 touch imp1 imp2 
-echo "babu sudhakar anna" >> imp1 
+echo " This is the output result" >> imp1 
+echo " i am updating the second line" >> imp2 
 
