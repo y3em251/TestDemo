b5bf1f03a46cad8d68cf549f9c1b4a5439b5669a
Santosh Kumar. Devara <santdeva@oradev.oraclecorp.com>
2018-07-25 08:37:23 +0530

    my 9th commit

--- mysecondjob.sh
+++ mysecondjob.sh
@@ -3,6 +3,6 @@
 echo "oh my God , what is happenning"
 cd /tmp
 touch imp1 imp2 
-echo " adbutham maha adbutham" >> imp1 
-echo " pulihora pakodi " >> imp2 
+echo " this is my first job in Jenkins" >> imp1 
+echo "  successfully executed in Target servers " >> imp2 
 
